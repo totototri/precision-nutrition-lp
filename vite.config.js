979dde6fc150977fac49a17846b6d9409cb9cef1
totototri <tawatokyo@gmail.com>
2025-09-23 18:40:59 +0900

    chore: set vite base for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,8 @@
-// vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      "/create-checkout-session": "http://localhost:4242",
-      "/create-subscription-session": "http://localhost:4242",
-    },
-  },
+  base: "/precision-nutrition-lp/", // ✅ これ
 });
 
